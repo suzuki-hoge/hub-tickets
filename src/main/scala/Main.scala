964766583b7b92fff015545e7350a327b738e235
suzuki-hoge <user.ryo@gmail.com>
2020-05-31 00:39:20 +0900

    implement refer attrs.

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -1,9 +1,10 @@
 import attrs.transfer.AttrsRepositoryImpl
 import attrs.transfer.git_hub.GitHubMock
 import attrs.transfer.zen_hub.ZenHubMock
-import cask.{MainRoutes, Request, post}
+import cask.{MainRoutes, Request, get, post}
 import command.transfer.issue.IssueRepositoryImpl
 import store.{Config, Store}
+import ujson.{Arr, Obj}
 
 object Main extends MainRoutes {
 
@@ -15,6 +16,18 @@ object Main extends MainRoutes {
 
   Config.initialize("~/Documents/tmp")
   Store.initialize(attrs)
+
+  @get("/attrs/labels") def labels(request: Request): String = Arr(
+    attrs.labels.map(label => Obj("name" -> label.name.v, "color" -> label.color.v)): _*
+  ).render()
+
+  @get("/attrs/assignees") def assignees(request: Request): String = Arr(
+    attrs.assignees.map(assignee => Obj("name" -> assignee.name.v, "icon" -> assignee.icon.v)): _*
+  ).render()
+
+  @get("/attrs/pipelines") def pipelines(request: Request): String = Arr(
+    attrs.pipelines.map(pipeline => Obj("name" -> pipeline.name.v)): _*
+  ).render()
 
   @post("/command/issue/create")
   def create(request: Request): String = {
